15916c88ecc9f46540762fe4754340e12918d5f3
Yagnesh L P <yagneshlp@gmail.com>
2020-12-31 14:02:28 +0530

    made some minor changes idr

--- src/components/Editmyraw.js
+++ src/components/Editmyraw.js
@@ -18,7 +18,7 @@ class editmyraw extends React.Component{
         return (   
             <div>
             <div className="bradcam_area">
-                <div className="single_bradcam  d-flex align-items-center bradcam_bg_1 overlay imageBGcontact">
+                <div className="single_bradcam  d-flex align-items-center bradcam_bg_1 overlay imageBGeditmyraw">
                     <div className="container">
                             <div className="row align-items-center justify-content-center">
                             <div className="col-xl-12">
@@ -29,7 +29,7 @@ class editmyraw extends React.Component{
                             <section >
                                     <div className="container box_1170 text-center wow fadeInLeft"  data-wow-duration="1s" data-wow-delay=".3s">                                       
                                         <p className="sample-text">
-                                        Would you like us to cover your event? Got any queries? Contact us
+                                        From draft to craft
                                         </p>                                        
                                     </div>
                                 </section>
@@ -40,8 +40,15 @@ class editmyraw extends React.Component{
             <section className="contact-section">
                 <div className="container">
                     <div className="row">
+                        <div className="col-lg-8 offset-lg-1">
+                            <h3 className="contact-title">Fill this form to participate</h3>
+                            <EditMyRaw />
+                        </div>
                         <div className="col-lg-3"> 
-                        <h2 className="contact-title">Contact us</h2>                          
+                            <h2 className="contact-title">Rulebook</h2>                           
+                            <p className="sample-text" >Read the rulebook <a href="" target="_blank">here</a> before your submit the images</p> 
+                            <br/>                                                   
+                            <h2 className="contact-title">Contact us</h2>                          
                             <div className="media contact-info">
                                 <span className="contact-info__icon"><i className="ti-mobile"></i></span>
                                 <div className="media-body">
@@ -52,26 +59,9 @@ class editmyraw extends React.Component{
                                     <h3>Anvar M</h3>
                                     <a href="tel:+91 81240 04834">+91 8124004834</a>                                    
                                 </div>
-                            </div>
-                            <div className="media contact-info">
-                                <span className="contact-info__icon"><i className="ti-email"></i></span>
-                                <div className="media-body">
-                                    <a href="mailto:pixelbugnittrichy@gmail.com"><h3>pixelbugnittrichy@gmail.com</h3></a>
-                                    {/* <p>Send us your query anytime!</p> */}
-                                </div>
-                            </div>
-                            <div className="media contact-info">
-                                <span className="contact-info__icon"><i className="ti-location-arrow"></i></span>
-                                <div className="media-body">
-                                    <h3>NIT Trichy</h3>
-                                    <p>Tanjavur Highway, 620015</p>
-                                </div>
-                            </div>
+                            </div>                           
                         </div>
-                        <div className="col-lg-8 offset-lg-1">
-                        <h3 className="contact-title">Fill this form to participate</h3>
-                            <EditMyRaw />
-                        </div>
+
                     </div>
                 </div>
             </section>
